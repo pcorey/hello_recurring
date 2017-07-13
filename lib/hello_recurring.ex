8d49a005182fd2fd9b1ca71b08885d422d3d54b3
Pete Corey <petecorey@gmail.com>
2017-07-13 08:15:48 -0400

    First commit

---
+++ lib/hello_recurring.ex
@@ -0,0 +1,18 @@
+defmodule HelloRecurring do
+  @moduledoc """
+  Documentation for HelloRecurring.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HelloRecurring.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hello_recurring_test.exs
@@ -0,0 +1,8 @@
+defmodule HelloRecurringTest do
+  use ExUnit.Case
+  doctest HelloRecurring
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
